580f9dd3e258e0188052e96efcd57f94376de3ba
DOAMAIS <doamais_ifpe@outlook.com>
2017-09-05 19:21:59 -0300

    05/09 com fotos da gente

---
+++ quemsomos.php
@@ -0,0 +1,91 @@
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="../_css/estilo.css">]
+  <title>Doamais</title>
+  </head>
+
+<body>
+  <style>
+    #menu {
+      background-color: #277554;
+    }
+
+    #botao {
+      background-color: #277554;
+      width: 200px;
+      color: white;
+    }
+
+    #container {
+      padding-top: 50px;
+    }
+  </style>
+  <nav class="navbar navbar-expand-md text-center fixed-top  w-100 p-1 navbar-inverse text-uppercase" id="menu">
+    <div class="container">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-center" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="inicial.php">Página Inicial <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cadastro.php">Cadastre-se</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="quemsomos.php">Quem somos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="text-center d-flex h-100 align-items-center" style="background-image: url(&quot;../green-gradient-wallpaper-1.jpg&quot;);">
+    <div class="container1" id="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-9/11828807_725952320865052_2900670184860484656_n.jpg?oh=d792336df189887aa94aaf220b54005f&oe=5A4F1F8F" class="d-block mx-auto rounded-circle img-fluid my-3">
+          <h3>Gabriel Victor</h3>
+          <p>Web Developer</p>
+          <div class="col-md-12">
+            <p>Não confia nas suas capacidades de desenvolvimento web
+              <br> mas sempre está pronto para ajudar a equipe no projeto e na vida.
+              Se preocupa com o projeto de forma geral.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <img src="https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-9/21369408_1767907146570980_5700583709319109555_n.jpg?oh=e675178d226ed1d1ad51627a21931906&oe=5A21CEEB" class="d-block mx-auto rounded-circle img-fluid my-3">
+          <h3>Mikele Guedes</h3>
+          <p>&nbsp;UI UX Developerr</p>
+          <div class="col-md-12">
+            <p>Gosta de opinar na aparência do projeto e procura estudar 
+              <br>como deixar uma aplicação bonita e funcional. 
+              <br>Se preocupa com o banco de dados
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <img src="https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-9/21271323_1767908389904189_523524269144081897_n.jpg?oh=e5dc9f79725946ef21db338e8a9d8853&oe=5A1680E8" class="d-block mx-auto rounded-circle img-fluid my-3">
+          <h3>Edmarcos Lima</h3>
+          <p>Back-end Developer</p>
+          <div class="col-md-12">
+            <p>Gosta de desafios e se irrita quando algo não sai como esperado. Entende de programação orientada à objeto.
+              Está ligado ao código e servidores
+              </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+  <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
+</body>
+
+</html>
