73d2f557bab46ecf25d00d08316c8c1025cf4c45
DOAMAIS <doamais_ifpe@outlook.com>
2017-09-11 18:35:12 -0300

    11/09

--- usuario.sql
+++ usuario.sql
@@ -1,5 +1,5 @@
 mysql> CREATE USER 'alterador'@'localhost' IDENTIFIED BY '123456';
 
-mysql> GRANT ALL PRIVILEGES ON DOAMAIS.* TO 'alterador'@'localhost';
+mysql> GRANT insert, select, update ON DOAMAIS.* TO 'alterador'@'localhost';
 
 
