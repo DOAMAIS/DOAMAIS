8b8f1b1effd28049bcc1a588541884d624a86871
DOAMAIS <doamais_ifpe@outlook.com>
2017-09-12 20:05:17 -0300

    postocoleta2.php

---
+++ postocoleta2.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <title>Pesquisar doador</title>
+  <meta name="generator" content="Bootply">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <link href="css/styles.css" rel="stylesheet">
+  <script src="ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/scripts.js"></script>
+  <script type="text/javascript">
+    function mostra(){
+      document.getElementsByTagname("table").style.display = "block";
+    }
+  </script>
+</head>
+
+<body>
+  <style>
+    #main {
+      background-image: url("../green-gradient-wallpaper-1.jpg");
+    }
+
+    #nav_cor {
+      background-color: #277554;
+    }
+
+    #botao {
+      background-color: #277554;
+      width: 90px;
+      color: white;
+    }
+
+    #sidebar {
+      background-color: #277554;
+    }
+
+    #imagem_logo {
+      width: 50px;
+      height: auto;
+      margin-top: 6px;
+      margin-left: 6px;
+    }
+
+    .profile-header-container {
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    .profile-header-img {
+      padding: 54px;
+    }
+
+    .profile-header-img>img.img-circle {
+      width: 120px;
+      height: 120px;
+      border: 2px solid #51D2B7;
+    }
+
+    .profile-header {
+      margin-top: 43px;
+    }
+
+    .rank-label-container {
+      margin-top: -19px;
+      text-align: center;
+    }
+
+    .label.label-default.rank-label {
+      background-color: rgb(81, 210, 183);
+      padding: 5px 10px 5px 10px;
+      border-radius: 27px;
+    }
+  </style>
+  <!-- grid da página inteira -->
+  <div class="column col-sm-12 col-xs-11" id="main">
+    <!-- barra de navegação -->
+    <div class="navbar navbar navbar-static-top" id="nav_cor">
+      <div class="navbar-header" id="nav_cor"> <span class="icon-bar "></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+        <img src="pica.png" id="imagem_logo"> </div>
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="index.php"> Página Inicial</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar">
+        <li class="dropdown">
+          <a href="altera_cadastro.php">Alterar Dados</a>
+        </li>
+      </ul> <label class="w-100 text-left m-1"></label>
+      <ul class="nav navbar-nav navbar pull-right">
+        <li class="dropdown">
+          <a href="logout.php">
+            <button class="btn btn-success">Sair</button>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- /barra de navegação -->
+    <div class="container">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h1 class="text-center m-2">Postos de coleta</h1>
+          <div class="section my-1 py-1">
+            <div class="container">
+              <div class="py-1  section">
+                <div class="row">
+                  <div class="col-md-4">
+                    <br>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.3864736170967!2d-34.897439585220155!3d-8.062003094195212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab18c4d3c7d39f%3A0xef85579d30319525!2sHemato+Banco+de+Sangue!5e0!3m2!1spt-BR!2sbr!4v1499983708374"
+                      width="350" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4">
+                      <div class="panel panel-heading">
+                        <h1 class="text-left text-danger"> HEMATO </h1>
+                        <p> <b>Endereço:</b> Av. Lins Petiti, 264 - Boa Vista, Recife - PE, 50070-230
+                          <br> <b>Telefone:</b> (81) 3972-4050
+                          <br> <b>Horário:</b> 07:00h às 18:00h </p>
+                      </div>
+                    </div>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    
+                   
+                   
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
